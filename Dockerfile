a242837ee3fd434ab62e8dc0070f4e17bdc347d9
Frode Anonsen <frode@anonsen.org>
2020-10-03 16:16:28 +0200

    Docker build and push

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ COPY build/libs/trip-1.0-SNAPSHOT-all.jar /opt/app.jar
 
 EXPOSE 8080
 
+ARG JAR_FILE
+COPY $JAR_FILE /opt/app.jar
 WORKDIR /opt
 CMD ["java", "-XshowSettings:vm", "-XX:+PrintFlagsFinal", "-XX:MaxRAMPercentage=70", "-Djava.security.egd=file:/dev/./urandom", "-Djava.awt.headless=true", "-jar", "/opt/app.jar"]
-
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
 
@@ -5,6 +7,8 @@ plugins {
   kotlin("jvm") version "1.4.10"
   application
   idea
+  jacoco
+  id("project-report")
   id("com.github.johnrengelman.shadow") version "6.0.0"
   id("io.gitlab.arturbosch.detekt") version "1.14.1"
   id("org.jlleitschuh.gradle.ktlint") version "9.4.0"
@@ -13,6 +17,9 @@ plugins {
 }
 group = "stavangler.bratur.services"
 version = "1.0-SNAPSHOT"
+val dockerRepositoryName = "stavangler"
+val dockerRepository = "$dockerRepositoryName.azurecr.io"
+val dockerImagePrefix = "$dockerRepositoryName.azurecr.io/bratur"
 
 repositories {
   mavenCentral()
@@ -37,11 +44,90 @@ dependencies {
   implementation("ch.qos.logback.contrib:logback-json-classic:$logbackJsonVersion")
   implementation("ch.qos.logback.contrib:logback-jackson:$logbackJsonVersion")
 }
-tasks.withType<KotlinCompile> {
-  kotlinOptions.jvmTarget = "13"
+
+tasks {
+  withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "13"
+  }
+
+  // Global tasks configuration
+  withType<GradleBuild> {
+    startParameter.showStacktrace = ShowStacktrace.ALWAYS
+  }
+
+  withType<JacocoReport> {
+    reports {
+      xml.apply {
+        isEnabled = true
+      }
+      html.apply {
+        isEnabled = true
+      }
+    }
+  }
+
+  // Always format first
+  "ktlintKotlinScriptCheck" { dependsOn(":ktlintFormat") }
+
+  withType<Test> {
+    useJUnitPlatform {
+      // systemProperty("spring.datasource.url", "jdbc:postgresql://localhost:45432/kotlink")
+    }
+    testLogging.apply {
+      events("started", "passed", "skipped", "failed")
+      exceptionFormat = TestExceptionFormat.SHORT
+      showCauses = false
+      showExceptions = false
+      showStackTraces = false
+      showStandardStreams = false
+      info.events = setOf(TestLogEvent.FAILED, TestLogEvent.SKIPPED)
+    }
+    afterSuite(
+      KotlinClosure2<TestDescriptor, TestResult, Unit>({ descriptor, result ->
+        if (descriptor.parent == null) {
+          logger.lifecycle(
+            "Tests run: ${result.testCount}, " +
+              "Failures: ${result.failedTestCount}, " +
+              "Skipped: ${result.skippedTestCount}"
+          )
+        }
+        Unit
+      })
+    )
+  }
+
+  register<Exec>("dockerBuild") {
+    group = "build"
+    description = "Build Docker image using Dockerfile in project root dir"
+    doFirst { logger.lifecycle("Building docker image ${project.name}:${project.version}") }
+    commandLine(
+      "docker",
+      "build",
+      "--build-arg",
+      "JAR_FILE=build/libs/${project.name}-${project.version}.jar",
+      "-t",
+      "$dockerImagePrefix/${project.name}:${project.version}",
+      "."
+    )
+  }
+
+  register<Exec>("dockerPush") {
+    group = "publish"
+    description = "Publish Docker image"
+    doFirst { logger.lifecycle("Pushing docker image ${project.name}:${project.version}") }
+    commandLine("docker", "push", "$dockerImagePrefix/${project.name}:${project.version}")
+    dependsOn(":dockerBuild")
+  }
 }
+
 application {
   mainClassName = "ServerKt"
+}
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_13
+  withJavadocJar()
+  withSourcesJar()
 }
 
 idea {
